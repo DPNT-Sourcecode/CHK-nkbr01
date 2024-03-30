cd7d3844dee5af521dff65db44d74c86f307d06f
sbx_user1051 <sbx_user1051@169.254.22.233>
2024-03-30 16:44:22 +0000

    Sat Mar 30 16:44:22 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,31 +9,32 @@ discount_map = {"A": (3, 130), "B": (2, 45)}
 
 promotion_map = {"E": (2, "B")}
 
-def convert_string_to_cart(self, s: str) -> dict:
-    cart = Cart()
-    Counter(s)
-    return cart
+
 
 class Cart:
     cart = {}
     total = 0
 
-    def __init__(self, skus):
-        convert_string_to_cart 
+    # def __init__(self, skus):
+    #     convert_string_to_cart 
 
-    def calculate_discounted_price(self, cart_quantity: int, full_price: int, sale_quantity: int, sale_price: int):
-        quantity = cart_quantity // sale_quantity
-        remainder = cart_quantity % sale_quantity
-        total_price = quantity * sale_price + remainder * full_price
-        return total_price
+    def convert_string_to_cart(self, s: str) -> dict:
+        self.cart = Counter(s)
+        return self.cart
+
+def calculate_discounted_price(cart_quantity: int, full_price: int, sale_quantity: int, sale_price: int):
+    quantity = cart_quantity // sale_quantity
+    remainder = cart_quantity % sale_quantity
+    total_price = quantity * sale_price + remainder * full_price
+    return total_price
                 
 
 def checkout(skus: str) -> int:
     cart = Cart()
-    .convert_string_to_cart(skus)
+    cart.convert_string_to_cart(skus)
     total = 0
     try:
-        for item, quantity in cart.items():
+        for item, quantity in cart.cart.items():
             if item in discount_map:
                 full_price = price_map[item]
                 sale_quantity, sale_price = discount_map[item]
@@ -43,3 +44,4 @@ def checkout(skus: str) -> int:
     except KeyError:
         return -1
     return total
+
