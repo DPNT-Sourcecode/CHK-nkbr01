4ca08ed151637aa6a044bb5e2d507231f64c3799
sbx_user1051 <sbx_user1051@169.254.22.233>
2024-03-30 15:37:59 +0000

    Sat Mar 30 15:37:59 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,15 +3,13 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 
-from collections import defaultdict
-def checkout(skus):
-    
-    price_map = {"A": 50, "B": 30, "C": 20, "D": 15}
+price_map = {"A": 50, "B": 30, "C": 20, "D": 15}
 
+from collections import defaultdict
+    
 def convert_string_to_cart(s: str) -> dict:
     cart = defaultdict(int)
     n = 0
-    #A2B4C23D
     while s:
         c = s[0]
         if c.isdigit():
@@ -27,3 +25,10 @@ def convert_string_to_cart(s: str) -> dict:
                 cart[c] = 1
         s = s[1:]
     return cart
+
+def checkout(skus):
+    cart = convert_string_to_cart(skus)
+    total = 0
+    for item, quantity in cart.items():
+        total += price_map[item] * quantity
+    return total
