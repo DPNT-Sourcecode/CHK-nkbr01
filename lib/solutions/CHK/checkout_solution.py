30c32184c1892b5c611a10461e3d444d678b1f7a
sbx_user1051 <sbx_user1051@169.254.22.233>
2024-03-30 15:28:59 +0000

    Sat Mar 30 15:28:59 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,10 +2,22 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+from collections import defaultdict
 def checkout(skus):
     
     price_map = {"A": 50, "B": 30, "C": 20, "D": 15}
 
-    return price_map[skus[0]]
+    def convert_string_to_cart(s: str) -> dict:
+        cart = defaultdict(int)
+        #A2B4C23D
+        while s:
+            c = s[0]
+            if c.isdigit():
+
+            else:
+
+
+            s = s[1:]
 
 
