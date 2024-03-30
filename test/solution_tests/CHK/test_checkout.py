fb230057b454d811f9fabeb11a7918ce01923365
sbx_user1051 <sbx_user1051@169.254.22.233>
2024-03-30 18:11:23 +0000

    Sat Mar 30 18:11:23 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -29,15 +29,19 @@ class TestCheckout():
 
     def test_free_B_with_two_E(self):
         cart = checkout_solution.Cart("EEB")
+        cart.apply_promotion()
         assert cart.calculate_cart_total() == 110
         assert cart.cart == {"B": 2, "E": 2}
 
     def test_free_B_with_two_E_1(self):
         cart = checkout_solution.Cart("EE")
+        cart.apply_promotion()
         assert cart.calculate_cart_total() == 80
         assert cart.cart == {"B": 1, "E": 2}
 
     def test_free_B_with_two_E_2(self):
         cart = checkout_solution.Cart("AEEEBB")
+        cart.apply_promotion()
         assert cart.calculate_cart_total() == 215
         assert cart.cart == {"A": 1, "B": 3, "E": 3}
+
