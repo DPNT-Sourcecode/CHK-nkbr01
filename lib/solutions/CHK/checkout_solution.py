efd8b335f34db6caa1f7a649bbb053464a7a4982
sbx_user1051 <sbx_user1051@169.254.22.233>
2024-03-30 15:22:59 +0000

    Sat Mar 30 15:22:59 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,5 +4,8 @@
 # skus = unicode string
 def checkout(skus):
     
-    price_map = {"A": 50, }
+    price_map = {"A": 50, "B": 30, "C": 20, "D": 15}
+
+    return price_map[skus[0]]
+
 
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -4,3 +4,6 @@ from solutions.CHK import checkout_solution
 class TestCheckout():
     def test_single_item_returns_single_price(self):
         assert checkout_solution.checkout("A") == 50
+
+    def test_multiple_items_returns_total_price(self):
+        assert checkout_solution.checkout("AB") == 80
