fd381813d787f2282b557b36704acf093c1e4a5a
sbx_user1051 <sbx_user1051@169.254.73.133>
2024-04-02 17:49:04 +0000

    Tue Apr 02 17:49:04 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -94,80 +94,36 @@ class TestCheckout():
     @pytest.mark.parametrize(
             "skus, total",
             [
-                ("NNNM") == 120,
-                ("RRRQ") == 150,
-                ("UUUU") == 120,
-                ("HHHHH") == 45,
-                ("HHHHHHHHHH") == 80,
-                ("PPPPP") == 200,
-                ("QQQ") == 80,
-                ("VV") == 90,
-                ("VVV") == 130,
-                ("NNN") == 120
+                ("NNNM", 120),
+                ("RRRQ", 150),
+                ("UUUU", 120),
+                ("HHHHH", 45),
+                ("HHHHHHHHHH", 80),
+                ("PPPPP", 200),
+                ("QQQ", 80),
+                ("VV", 90),
+                ("VVV", 130),
+                ("NNN", 120),
+                ("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 837),
+                ("ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ", 1602),
             ],
     )
-    def test_multiple_item__new_promo_and_multibuy(self, skus, total):
+    def test_multiple_item_new_promo_and_multibuy(self, skus, total):
         assert checkout_solution.checkout(skus) == total
 
-
-
-    def test_discount_applied_multiple_item_19(self):
-        assert checkout_solution.checkout("NNNM") == 120
-
-    def test_discount_applied_multiple_item_20(self):
-        assert checkout_solution.checkout("RRRQ") == 150
-
-    def test_discount_applied_multiple_item_21(self):
-        assert checkout_solution.checkout("UUUU") == 120
-
-    def test_discount_applied_multiple_item_22(self):
-        assert checkout_solution.checkout("HHHHH") == 45
-
-    def test_discount_applied_multiple_item_23(self):
-        assert checkout_solution.checkout("HHHHHHHHHH") == 80
-
-    def test_discount_applied_multiple_item_24(self):
-        assert checkout_solution.checkout("PPPPP") == 200
-
-    def test_discount_applied_multiple_item_25(self):
-        assert checkout_solution.checkout("QQQ") == 80
-
-    def test_discount_applied_multiple_item_26(self):
-        assert checkout_solution.checkout("VV") == 90
-
-    def test_discount_applied_multiple_item_27(self):
-        assert checkout_solution.checkout("VVV") == 130
-
-    def test_discount_applied_multiple_item_28(self):
-        assert checkout_solution.checkout("NNN") == 120
-
-    def test_discount_applied_multiple_item_29(self):
-        assert checkout_solution.checkout("G") == 20
-
-    def test_discount_applied_multiple_item_30(self):
-        assert checkout_solution.checkout("ABCDEFGHIJKLMNOPQRSTUVWXYZ") == 837
-
-    def test_discount_applied_multiple_item_31(self):
-        assert checkout_solution.checkout("ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ") == 1602
-
-    def test_discount_applied_multiple_item_32(self):
-        assert checkout_solution.checkout("STX") == 45
-
-    def test_discount_applied_multiple_item_33(self):
-        assert checkout_solution.checkout("SSS") == 45
-
-    def test_discount_applied_multiple_item_34(self):
-        assert checkout_solution.checkout("STXYZS") == 90
-
-    def test_discount_applied_multiple_item_35(self):
-        assert checkout_solution.checkout("XYZAB") == 125
-
-    def test_discount_applied_multiple_item_36(self):
-        #choose the most expensive for the group discount
-        assert checkout_solution.checkout("STXYZ") == 82
-
-    def test_discount_applied_multiple_item_37(self):
-        assert checkout_solution.checkout("SSZYZ") == 85
+    @pytest.mark.parametrize(
+            "skus, total",
+            [
+                ("STX", 45),
+                ("SSS", 45),
+                ("STXYZS", 90),
+                ("XYZAB", 125),
+                ("STXYZ", 82),
+                ("SSZYZ", 85)
+            ],
+    )
+    def test_multiple_item_groupbuy(self, skus, total):
+        assert checkout_solution.checkout(skus) == total
 
 
 
