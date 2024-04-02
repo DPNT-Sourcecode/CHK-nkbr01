27a6eee5711a6e14caa787477bdc47aaaeb9aef7
sbx_user1051 <sbx_user1051@169.254.73.133>
2024-04-02 17:34:04 +0000

    Tue Apr 02 17:34:04 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,7 +1,16 @@
 from solutions.CHK import checkout_solution
-
+import pytest
 
 class TestCheckout():
+
+    @pytest.mark.parametrize(
+            "skus, total",
+            [
+                (),
+                (),
+            ],
+    )
+
     def test_single_item_returns_single_price(self):
         assert checkout_solution.checkout("A") == 50
 
